301a020d0aa40b4b598dbe22b451d3dc31a3ef52
rooparsh <rooparshkalia@gmail.com>
2021-10-17 17:38:47 +0530

    db key updated

--- src/main/kotlin/com/darklabs/db/DatabaseConnectionFactory.kt
+++ src/main/kotlin/com/darklabs/db/DatabaseConnectionFactory.kt
@@ -1,21 +1,12 @@
 package com.darklabs.db
 
 import org.ktorm.database.Database
-import java.net.URI
 
 class DatabaseConnectionFactory {
     lateinit var database: Database
-    private val uri = URI(System.getenv("DATABASE_URL"))
-    private val uriArray = uri.userInfo.split(":").toTypedArray()
-    private val userName = uriArray[0]
-    private val password = uriArray[1]
-
     fun init() {
         database = Database.connect(
-            url = "jdbc:postgresssql://${uri.host}:${uri.port}${uri.path}",
-            driver = "org.postgresql.Driver",
-            user = userName,
-            password = password
+            url = System.getenv("JDBC_DATABASE_URL")
         )
     }
 }
